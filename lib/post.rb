18945c312b21276673053b527a702f16277e263b
Ben <henriquez.ben@gmail.com>
2016-10-03 19:16:45 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post
+  attr_accessor :title, :author
+  # def author
+  #   # author = Author.new
+  #   # author.post = self
+  # end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+  # def artist
+  #   artist = Artist.new
+  #   self.artist = artist
+  #   artist
+  # end
+end
